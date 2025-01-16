d29092daa5da339e6d7f116839c7143b23dedec8
Michal Scienski <mscienski@gmail.com>
2025-01-16 05:48:44 +0000

    feat(board): add board structure

--- test/islands_engine/coordinate_test.exs
+++ test/islands_engine/coordinate_test.exs
@@ -6,11 +6,11 @@ defmodule CoordinateTest do
 
   describe "new/0" do
     test "creates a coordinate" do
-      assert {:ok, %Coordinate{row: 1, col: 1}} = Coordinate.new(1, 1)
+      assert {:ok, %Coordinate{row: 1, col: 1}} == Coordinate.new(1, 1)
     end
 
     test "returns invalid coordinate when out of range" do
-      assert {:error, :invalid_coordinate} = Coordinate.new(1, -1)
+      assert {:error, :invalid_coordinate} == Coordinate.new(1, -1)
     end
   end
 end
